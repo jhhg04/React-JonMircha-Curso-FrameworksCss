f7a63dba89f0947fba12bc2bfa965802de4caf4b
jhhg0 <jhherrera@ccss.sa.cr>
2022-06-09 18:48:44 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import MaterialUI from './components/MaterialUI';
+// import ReactBootstrap from './components/ReactBootstrap';
 // import Bootstrap from './components/Bootstrap';
-import Bulma from './components/Bulma';
+// import Bulma from './components/Bulma';
 
 function App() {
   return (
     <div>
       <h1>Frameworks CSS con React</h1>
       {/* <Bootstrap /> */}
-      <Bulma />
+      {/* <Bulma /> */}
+      {/* <ReactBootstrap /> */}
+      <MaterialUI />
     </div>
   );
 }
---
+++ src/components/MaterialUI.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MediaCard from './MediaCard';
+
+export default function MaterialUI() {
+  return (
+    <>
+      <h2>MaterialUI</h2>
+      <MediaCard />
+    </>
+  );
+}
